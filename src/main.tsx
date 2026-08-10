2ad3bd002dbfb88483e2f3c738989f7ae0324819
vornicx <vornicx@gmail.com>
2026-08-10 16:05:26 +0200

    Load Archic phone contact styles

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ import './index.css'
 import './styles/studio.css'
 import './styles/archic-site.css'
 import './styles/archic-contrast.css'
+import './styles/archic-phone.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
