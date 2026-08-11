05c473fc4094baab94bd76107718ace77159488f
vornicx <vornicx@gmail.com>
2026-08-11 23:38:56 +0200

    add Antero contact details

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -1,2 +1,9 @@
 export const CONTACT_PHONE = '+34644768515'
 export const CONTACT_PHONE_DISPLAY = '+34 644 76 85 15'
+
+export const ANTERO_CONTACT = {
+  name: 'Antero',
+  email: 'antero@archic.es',
+  phone: '+34687503850',
+  phoneDisplay: '+34 687 50 38 50',
+} as const
